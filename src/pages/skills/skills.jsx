143f233521aacb15c50e25850adfbb5fef53a5c2
Hafeez <fasthafeezahamed21@gmail.com>
2026-01-15 16:06:54 +0530

    updated

--- src/pages/skills/skills.jsx
+++ src/pages/skills/skills.jsx
@@ -19,66 +19,66 @@ const Skills = () => {
             <div className="skils-title">
                 <h1>Skills & Expertise</h1>
             </div>
-            <div className="skills-item">
+            <div className="skills-text">
                 <h1>Skills</h1>
 
-                <div className="skills-container">
-                    <div className="skills-card">
+                <div className="skills-wrapper">
+                    <div className="skills-item">
                         <img src={htmlLogo} />
                         <h3>HTML 5</h3>
                         <p>Years of Experience: <strong>Intermediate</strong></p>
                     </div>
-                    <div className="skills-card">
+                    <div className="skills-item">
                         <img src={cssLogo} />
                         <h3>CSS 3</h3>
                         <p>Years of Experience: <strong>Intermediate</strong></p>
                     </div>
-                    <div className="skills-card">
+                    <div className="skills-item">
                         <img src={jsLogo} />
                         <h3>JAVASCRIPT</h3>
                         <p>Years of Experience: <strong>Intermediate</strong></p>
                     </div>
-                    <div className="skills-card">
+                    <div className="skills-item">
                         <img src={reactLogo} />
                         <h3>REACT JS</h3>
                         <p>Years of Experience: <strong>Intermediate</strong></p>
                     </div>
-                    <div className="skills-card">
+                    <div className="skills-item">
                         <img src={bootstrapLogo} />
                         <h3>BOOTSTRAP</h3>
                         <p>Years of Experience: <strong>Intermediate</strong></p>
                     </div>
-                    <div className="skills-card">
+                    <div className="skills-item">
                         <img src={javaLogo} />
                         <h3>JAVA</h3>
                         <p>Years of Experience: <strong>Beginner</strong></p>
                     </div>
-                    <div className="skills-card">
+                    <div className="skills-item">
                         <img src={mysqlLogo} />
                         <h3>MY SQL</h3>
                         <p>Years of Experience: <strong>Intermediate</strong></p>
                     </div>
-                    <div className="skills-card">
+                    <div className="skills-item">
                         <img src={githubLogo} />
                         <h3>GIT HUB</h3>
                         <p>Years of Experience: <strong>Intermediate</strong></p>
                     </div>
-                    <div className="skills-card">
+                    <div className="skills-item">
                         <img src={canvaLogo} />
                         <h3>CANVA</h3>
                         <p>Years of Experience: <strong>Beginner</strong></p>
                     </div>
-                    <div className="skills-card">
+                    <div className="skills-item">
                         <img src={mswordLogo} />
                         <h3>MS WORD</h3>
                         <p>Years of Experience: <strong>1+</strong></p>
                     </div>
-                    <div className="skills-card">
+                    <div className="skills-item">
                         <img src={powerpointLogo} />
                         <h3>POWER POINT</h3>
                         <p>Years of Experience: <strong>Intermediate</strong></p>
                     </div>
-                    <div className="skills-card">
+                    <div className="skills-item">
                         <img src={msexcelLogo} />
                         <h3>MS EXCEL</h3>
                         <p>Years of Experience: <strong>1+</strong></p>
